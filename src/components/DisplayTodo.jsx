ba78a7c00c8bea2a9df89726de93bffed5969499
manzitresor <manzitre@gmail.com>
2024-08-15 11:42:41 +0200

    Add Display Todo from  Todo store

---
+++ src/components/DisplayTodo.jsx
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+import { MdDelete } from "react-icons/md";
+export default function DisplayTodo() {
+  const todos = useSelector((state)=> state.todos);
+  
+  return (
+    <>
+      <ul>
+          {todos.map((todo, index) => (
+            <div key={index} className="flex items-center border-b border-gray-200 py-5">
+              <input type="checkbox" checked={todo.completed} value={todo.completed}  className="mx-3 size-4 mt-1"></input>
+              <li className={`mr-auto text-2xl ${todo.completed? 'line-through': ''}`}>{todo.text}</li>
+              <div className="bg-gray-100 rounded-full h-11 w-11 flex items-center justify-center">
+                  <MdDelete className="text-red-500 text-2xl cursor-pointer"/>
+              </div>
+            </div>
+          ))}
+        </ul>
+    </>
+  )
+}
