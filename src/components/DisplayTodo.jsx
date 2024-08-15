ca6190695b3de3b597da42952958787f7d97495c
manzitresor <manzitre@gmail.com>
2024-08-15 13:09:09 +0200

    Add make completed task toggle

--- src/components/DisplayTodo.jsx
+++ src/components/DisplayTodo.jsx
@@ -1,17 +1,26 @@
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 import { MdDelete } from "react-icons/md";
+import { removeTodo } from "../feature/todoSlice";
+import { toggleTask } from "../feature/todoSlice";
+
 export default function DisplayTodo() {
   const todos = useSelector((state)=> state.todos);
-  
+  const dispatch = useDispatch();
   return (
     <>
       <ul>
           {todos.map((todo, index) => (
             <div key={index} className="flex items-center border-b border-gray-200 py-5">
-              <input type="checkbox" checked={todo.completed} value={todo.completed}  className="mx-3 size-4 mt-1"></input>
+              <input 
+                    type="checkbox" 
+                    checked={todo.completed} 
+                    value={todo.completed}  
+                    className="mx-3 size-4 mt-1"
+                    onChange={()=> dispatch(toggleTask(todo.id))}
+                    ></input>
               <li className={`mr-auto text-2xl ${todo.completed? 'line-through': ''}`}>{todo.text}</li>
               <div className="bg-gray-100 rounded-full h-11 w-11 flex items-center justify-center">
-                  <MdDelete className="text-red-500 text-2xl cursor-pointer"/>
+                  <MdDelete className="text-red-500 text-2xl cursor-pointer" onClick={()=> dispatch(removeTodo(todo.id))}/>
               </div>
             </div>
           ))}
--- src/feature/todoSlice.js
+++ src/feature/todoSlice.js
@@ -11,12 +11,19 @@ export const todoSlice = createSlice({
         addTodo: (state,actions) => {
             const todo = {
                 id: nanoid(),
+                completed: false,
                 text: actions.payload
             }
             state.todos.push(todo)
+        },
+        removeTodo: (state,actions) => {
+            state.todos = state.todos.filter((todo)=> todo.id !== actions.payload)
+        },
+        toggleTask: (state,actions) => {
+            state.todos = state.todos.map(todo => todo.id === actions.payload ? {...todo, completed: !todo.completed}: todo)
         }
     }
 })
 
-export const { addTodo } = todoSlice.actions
+export const { addTodo, removeTodo, toggleTask } = todoSlice.actions
 export default todoSlice.reducer
